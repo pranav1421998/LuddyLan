3db6f991f7c7710d0fbc64b55d0e0b70c90e3f08
venkatakalyanivemula <vevemula@iu.edu>
2023-09-20 15:19:50 -0400

    pushing react application

---
+++ socialnetwork/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello, This is a website for social network application.
+      </header>
+    </div>
+  );
+}
+
+export default App;
