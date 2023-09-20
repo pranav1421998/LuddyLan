b51c39ca5409540167ee9b767da065383316e41f
venkatakalyanivemula <vevemula@iu.edu>
2023-09-20 16:12:55 -0400

    created a login file

--- socialnetwork/src/App.js
+++ socialnetwork/src/App.js
@@ -1,11 +1,15 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Register} from './Register.js';
+import {Login} from './Login.js';
 
 function App() {
+  const {currentForm,setCurrentForm} = useState('login');
   return (
     <div className="App">
       <header className="App-header">
-        Hello, This is a website for social network application.
+        <Login></Login>
       </header>
     </div>
   );
---
+++ socialnetwork/src/Login.js
@@ -0,0 +1,21 @@
+import React,{useState} from "react";
+export const Login=()=>{
+    const {email,setEmail} = useState('');
+    const {password,setPass} = useState('');
+    const handleSubmit=()=>{
+      email.preventDefault();
+      console.log(email);
+    }
+    return(
+      <>
+        <form onSubmit={handleSubmit}>
+          <label for="email" >Enter your username</label><br></br>
+          <input type="email" value={email} placeholder="username@gmail.com" id="email" name="email"></input><br></br>
+          <label for="password" >Enter your password</label><br></br>
+          <input type="password" value={password} placeholder="*********" id="password" name="password"></input><br></br>
+          <button>Login</button>
+        </form>
+        <h6>Don't have an account?<button>Register</button></h6>
+      </>
+    )
+}
