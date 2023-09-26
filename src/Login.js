18698214f6dad3f1db19fabae649a1e9ef951e38
venkatakalyanivemula <vevemula@iu.edu>
2023-09-26 19:39:28 -0400

    changes made in login

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,5 @@
-import firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebaseConfig"; 
 import "firebase/auth";
 import React, { useState } from "react";
 import {GoogleButton} from 'react-google-button';
@@ -11,9 +12,10 @@ const Login = (props) => {
         console.log(email);
     }
   // Define the Google Sign-In method
+  const app = initializeApp(firebaseConfig);
   const signInWithGoogle = () => {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithPopup(provider).then((result) => {
+      const provider = new app.auth.GoogleAuthProvider();
+      app.auth().signInWithPopup(provider).then((result) => {
           const user = result.user;
           // Redirect or navigate to another page after successful login, if desired
           console.log("Logged in as:", user.displayName);
@@ -27,17 +29,15 @@ const Login = (props) => {
     <div className="auth-form-container">
             <h2>Login</h2>
             <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                <label htmlFor="password">password</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <label htmlFor="email">Username</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="username@iu.edu" id="email" name="Username" />
+                <label htmlFor="password">Password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="Password" />
                 <button type="submit">Log In</button>
             </form>
             <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+            <GoogleButton onClick={signInWithGoogle}></GoogleButton>
         </div>
-        <div className="login-container">
-             <button onClick={signInWithGoogle}>Sign in with Google</button>
-      </div>
     </>
   );
 };
