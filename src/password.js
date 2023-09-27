ffe2b82e5cca5d6f4024007d7547d4f11e9ed822
venkatakalyanivemula <vevemula@iu.edu>
2023-09-26 20:55:02 -0400

    ..

--- src/password.js
+++ src/password.js
@@ -12,9 +12,9 @@ export const CreatePassword = () => {
             <h2>Create Password</h2>
         <form className="createpassword-form" onSubmit={handleSavePassword}>
             <label htmlFor="password">Create a Password</label>
-            <input type="password" value={password} placeholder="********" id="password" name="password"></input>
+            <input type="password" onChange={(e) => setPassword(e.target.value)}value={password} placeholder="********" id="password" name="password"></input>
             <label htmlFor="confirmpassword">Confirm Password</label>
-            <input type="password" placeholder="********" value={confirmPass} id="confirmpassword" name="confirmpassword"></input>
+            <input type="password" onChange={(e) => setConfirmPass(e.target.value)}placeholder="********" value={confirmPass} id="confirmpassword" name="confirmpassword"></input>
             <button type="sumit" onClick={handleSavePassword}>Save Password</button>
         </form>
         </div>
