4582396ab0c96a639501465e61ef90383f89a80d
deeconp <dgprecht5352@gmail.com>
2023-09-29 17:24:43 -0400

    register page changes

--- src/Register.js
+++ src/Register.js
@@ -7,6 +7,9 @@ export const Register = () => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
     const [byear, setByear] = useState('');
+    const [first, setFirst] = useState('');
+    const [last, setLast] = useState('');
+    const [phone, setPhone] = useState('');
 
     const navigate = useNavigate();
 
@@ -38,10 +41,18 @@ export const Register = () => {
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="Password" />
                 
                 <label>Please fill out some basic info:</label>
-                <label htmlFor="byear">Birth Year: 
-                <input value={byear} onChange={(e) => setByear(e.target.value)}type="byear" placeholder="1990" id="byear" name="byer" />
+                <label htmlFor="first">First Name: 
+                <input value={first} onChange={(e) => setFirst(e.target.value)}type="first" placeholder="first" id="first" name="first" />
                 </label>
-        
+                <label htmlFor="last">Last Name: 
+                <input value={last} onChange={(e) => setLast(e.target.value)}type="last" placeholder="last" id="last" name="last" />
+                </label>
+                <label htmlFor="byear">Birth Year: 
+                <input value={byear} onChange={(e) => setByear(e.target.value)}type="byear" placeholder="1990" id="byear" name="byear" />
+                </label>
+                <label htmlFor="phone">Phone: 
+                <input value={phone} onChange={(e) => setPhone(e.target.value)}type="phone" placeholder="5556667777" id="phone" name="phone" />
+                </label>
                 <button type="submit" onClick={handleSubmit}>Create Account</button>
             </form>
             <button className="link-btn" onClick={() => navigate("/")}>Already have an account? Login.</button>
