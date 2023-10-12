61a93608d7baedc5200f7200ed03ef51176fb9e7
Mahajan, Pranav <pmahaja@iu.edu>
2023-10-12 17:04:23 -0400

    fetched details from the database

--- src/AllUsers.js
+++ src/AllUsers.js
@@ -1,23 +1,38 @@
-import React from "react";
 import './FriendRequests.css';
 import GridCards from "./GridCards";
 import SidebarFriends from "./SidebarFriends";
-const AllUsers = () => {
-    const data = [
-        { name: "User 1", profilePicture: "Images/user.jpg/" },
-        { name: "User 2", profilePicture: "Images/user.jpg" },
-        { name: "User 3", profilePicture: "Images/user.jpg" },
-        { name: "User 3", profilePicture: "Images/user.jpg" },
-        { name: "User 3", profilePicture: "Images/user.jpg" },
-        { name: "User 3", profilePicture: "Images/user.jpg" },
-        { name: "User 3", profilePicture: "Images/user.jpg" },
-        { name: "User 3", profilePicture: "Images/user.jpg" },
-        { name: "User 3", profilePicture: "Images/user.jpg" },
-        { name: "User 3", profilePicture: "Images/user.jpg" },
-        { name: "User 3", profilePicture: "Images/user.jpg" },
+import React, { useState, useEffect } from "react";
+import {auth, db, provider, storage} from "./firebaseConfig";
+import { collection, addDoc, doc, setDoc, getDocs } from 'firebase/firestore';
 
-        // Add more data as needed
+
+const AllUsers = () => {
+    const [userdata, setDocuments] = useState([]); 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const userCollRef = collection(db, 'users');
+                const userSnapshot = await getDocs(userCollRef);
+                const userDataArray = userSnapshot.docs.map(doc => doc.data());
+                setDocuments(userDataArray);
+            } catch (error) {
+                console.error("An error occurred while fetching data:", error);
+            }
+        };
+    
+        fetchData();
+    }, []);
+
+console.log(userdata);
+
+
+    const data = [ 
+        //{ name: userdata[0].firstName  , profilePicture: "Images/user.jpg/" }
     ];
+    for (let i=0; i<userdata.length; i++) {
+        data.push({name: userdata[i].firstName, profilePicture: "Images/user.jpg"})
+
+    }
 
     return (
         <div className="component">
