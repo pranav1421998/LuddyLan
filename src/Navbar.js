d919377989a9863f0e39cbff9bc06c000b9f23ac
venkatakalyanivemula <vevemula@iu.edu>
2023-10-15 23:37:24 -0400

    dashboard

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,36 +1,79 @@
+// src/components/Navbar.js
+
+ 
+
 import './Navbar.css'
+
+import icon from './Images/icon.png';
+
 import React, { useEffect } from "react";
+
+import { auth, db } from "./firebaseConfig";
+
+import { doc, getDoc, updateDoc} from 'firebase/firestore';
+
 import { useNavigate } from "react-router-dom";
-import icon from './Images/icon2.png';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import { faHome, faUser, faUsers, faComment, faGear, faSignOut } from '@fortawesome/free-solid-svg-icons';
 
+ 
+
 const Navbar = () => {
-  
-  const navigate = useNavigate();   
+
+ 
+
+  const navigate = useNavigate();  
+
+ 
 
   return (
 
+ 
+
       <nav className="navbar">
+
         {/* logo */}
+
         <div className="logo">
+
           <img src={icon} alt="Logo" />
+
         </div>
+
         {/* other webpage links */}
+
         <div className="pages">
+
           <p onClick={() => navigate('/dashboard')}><FontAwesomeIcon icon={faHome}/> Home</p>
+
           <p onClick={() => navigate('/FriendRequests')}><FontAwesomeIcon icon={faUsers}/> Friends</p>
+
           <p onClick={() => navigate('/chat')}><FontAwesomeIcon icon={faComment}/> Chat</p>
+
           <p onClick={() => navigate('/profile')}><FontAwesomeIcon icon={faUser}/> Profile</p>
+
           <p onClick={() => navigate('/profileSettings')}><FontAwesomeIcon icon={faGear}/> Settings</p>
+
         </div>
+
         {/* log out */}
+
         <div className="login">
+
           <p onClick={() => navigate('/')}><FontAwesomeIcon icon={faSignOut}/> Log out</p>
+
         </div>
+
       </nav>
+
     );
 
+ 
+
 };
+
+ 
 
 export default Navbar;
--- src/dashboard.js
+++ src/dashboard.js
@@ -1,26 +1,43 @@
-import './dashboard.css';
-import React, { useEffect, useState } from "react";
-import { doc, getDoc, getDocs, updateDoc, collection } from "firebase/firestore";
-import { onAuthStateChanged } from "firebase/auth";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faThumbsUp, faComment, faShare } from '@fortawesome/free-solid-svg-icons';
+import "./dashboard.css";
+
 import { db, auth } from "./firebaseConfig";
 import { Link, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+
+import {
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  collection,
+} from "firebase/firestore";
+
+import { onAuthStateChanged } from "firebase/auth";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faUser,
+  faThumbsUp,
+  faComment,
+  faShare,
+} from "@fortawesome/free-solid-svg-icons";
+import CreatePost from './CreatePost';
+import PollPopup from './CreatePoll';
 
 const Dashboard = () => {
-    const [userDetails, setUserDetails] = useState(null);
+  const [userDetails, setUserDetails] = useState(null);
+
+  const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
     const [showModal, setShowModal] = useState(false);
     const [showPopup, setShowPopup] = useState(false);
-    const [posts, setPosts] = useState([]);
-
-    const navigate = useNavigate();
 
     const openPollPopup = () => {
         setShowPopup(true);
     };
-
-    const ClosePollPopup = () => {
-        setShowPopup(false);
+    const ClosePollPopup=()=>{
+      setShowPopup(false);
     };
 
     const handleOpenModal = () => {
@@ -31,83 +48,114 @@ const Dashboard = () => {
         setShowModal(false);
     };
 
-    useEffect(() => {
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const collectionRef = collection(db, "posts");
 
-        const fetchPosts = async () => {
-            const collectionRef = collection(db, 'posts');
-            const querySnapshot = await getDocs(collectionRef);
-            const documentsData = [];
-            querySnapshot.forEach((doc) => {
-                documentsData.push({ id: doc.id, ...doc.data() });
+      const querySnapshot = await getDocs(collectionRef);
+
+      const documentsData = [];
+
+      querySnapshot.forEach((doc) => {
+        documentsData.push({ id: doc.id, ...doc.data() });
+      });
+
+      setPosts(documentsData);
+
+      documentsData.forEach((data) => {
+        // const collectionData = document.getElementById(data.id);
+        // console.log(collectionData.data);
+      });
+    };
+
+    fetchPosts();
+
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const userDocRef = doc(db, "users", user.email);
+
+        const userDoc = await getDoc(userDocRef);
+
+        if (userDoc.exists()) {
+          setUserDetails({ id: userDoc.id, ...userDoc.data() }); // Include the document ID in the user data
+
+          // Update a single field in the document
+
+          try {
+            await updateDoc(userDocRef, {
+              loggedIn: true,
             });
-            setPosts(documentsData);
-        };
-        fetchPosts();
 
-        const unsubscribe = onAuthStateChanged(auth, async (user) => {
-            if (user) {
-                const userDocRef = doc(db, "users", user.email);
-                const userDoc = await getDoc(userDocRef);
-                if (userDoc.exists()) {
-                    setUserDetails({ id: userDoc.id, ...userDoc.data() });
-                    try {
-                        await updateDoc(userDocRef, {
-                            loggedIn: true
-                        });
-                        window.status = true;
-                        console.log("Document successfully updated!");
-                    } catch (error) {
-                        console.error("Error updating document: ", error);
-                    }
-                } else {
-                    console.log("No such document!");
-                }
-            } else {
-                console.log("No user is signed in");
-            }
-        });
+            window.status = true;
 
-        return () => unsubscribe(); // Unsubscribe from the observer on component unmount
-    }, [auth, db]);
+            console.log("Document successfully updated!");
+          } catch (error) {
+            console.error("Error updating document: ", error);
+          }
+        } else {
+          console.log("No such document!");
+        }
+      } else {
+        console.log("No user is signed in");
+      }
+    });
+  }, [auth, db]);
 
-    return (
-            
+  return (
     <section className="main">
-        <div className="post-container">
-            <ul>
-            {posts.map((post) => (
+      <div className="post-container">
+        <ul>
+          {posts.map((post) => (
             <li key={post.id}>
-            <div className="post">
+              <div className="post">
                 {/* post header */}
+
                 <div className="post-header">
-                    <p className="user-icon"><FontAwesomeIcon icon={faUser}/></p>
-                <p className= "username">{post.ownerId}</p> 
+                  <p className="user-icon">
+                    <FontAwesomeIcon icon={faUser} />
+                  </p>
+
+                  <p className="username">{post.ownerId}</p>
                 </div>
+
                 {/* post caption */}
+
                 <div className="post-detail">
-                    <p style={{color: 'black'}}>{post.caption}</p>    
+                  <p style={{ color: "black" }}>{post.caption}</p>
                 </div>
+
                 {/* post section */}
+
                 <div className="post-feed">
-                    <img src={post.media} className="image-container" alt="Image" />
+                  <img
+                    src={post.media}
+                    className="image-container"
+                    alt="Image"
+                  />
                 </div>
+
                 {/* post buttons */}
+
                 <div className="detail-interactions">
-                    <button className="btn"><FontAwesomeIcon icon={faThumbsUp}/> Like</button>
-                    <button className="btn"><FontAwesomeIcon icon={faComment}/> Comment</button>
-                    <button className="btn"><FontAwesomeIcon icon={faShare}/> Share</button>
+                  <button className="btn">
+                    <FontAwesomeIcon icon={faThumbsUp} /> Like
+                  </button>
 
+                  <button className="btn">
+                    <FontAwesomeIcon icon={faComment} /> Comment
+                  </button>
+
+                  <button className="btn">
+                    <FontAwesomeIcon icon={faShare} /> Share
+                  </button>
                 </div>
-                
-            </div>
-                
+              </div>
             </li>
-            ))}
-            </ul>
-        </div>
+          ))}
+        </ul>
+      </div>
     </section>
-
-    );
+  );
 };
 
 export default Dashboard;
