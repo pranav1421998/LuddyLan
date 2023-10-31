e9b4f8de6b600591e1adb33847c63503449728fc
Mahajan, Pranav <pmahaja@iu.edu>
2023-10-31 13:35:57 -0400

    .

--- src/searchResults.js
+++ src/searchResults.js
@@ -6,10 +6,6 @@ const SearchResults = () => {
   return (
     <div className='main'>
       <h1>Search Results</h1>
-      <div>
-        <Link to="/search-results/users">Users</Link>
-        <Link to="/search-results/posts">Posts</Link>
-      </div>
       {/* Add your logic to display users or posts based on the selected tab here */}
     </div>
   );
